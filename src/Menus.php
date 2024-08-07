ab3f6136276d51bc321b311522990026da34ea7c
Muhammad Umer <mumer@updraftplus.com>
2024-08-07 20:48:55 +0500

    initial update

---
+++ src/Menus.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace WPEssential\Library;
+
+if ( ! \defined( 'ABSPATH' ) && ! \defined( 'WPE_REG_MENUS' ) )
+{
+	exit; // Exit if accessed directly.
+}
+
+final class Menus
+{
+	protected $add_menus    = [];
+	protected $remove_menus = [];
+
+	public function __construct () {}
+
+	public static function make ()
+	{
+		return new self();
+	}
+
+	public function add ( $args = [] )
+	{
+		$this->add_menus = array_merge( $this->add_menus, $args );
+
+		return $this;
+	}
+
+	public function remove ( $key = '' )
+	{
+		$this->remove_menus = array_push( $this->remove_menus, $key );
+
+		return $this;
+	}
+
+	public function init ()
+	{
+		$this->register();
+		$this->unregister();
+	}
+
+	protected function unregister ()
+	{
+		$menus = apply_filters( 'wpe/library/menus_remove', array_merge( $this->remove_menus, [] ) );
+		if ( ! empty( $menus ) )
+		{
+			foreach ( $menus as $menu )
+			{
+				unregister_nav_menu( $menu );
+			}
+		}
+	}
+
+	protected function register ()
+	{
+		$menus = apply_filters(
+			'wpe/library/menus_add',
+			array_merge( $this->add_menus, [
+				'primary_menu' => esc_html__( 'Primary Menu', 'wpessential' ),
+				'footer_menu'  => esc_html__( 'Footer Menu', 'wpessential' ),
+			] )
+		);
+
+		if ( ! empty( $menus ) )
+		{
+			register_nav_menus( $menus );
+		}
+	}
+}
